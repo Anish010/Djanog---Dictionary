822490bbba0e38cb2e8aae3e237df4fd4eaa7d49
Anish010 <anishmohanty101@gmail.com>
2023-04-20 16:56:41 +0530

    Added Bootstrap and optimized end cases

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
-from .forms import WordForm
+
 # Create your views here.
 
 
@@ -28,48 +28,47 @@ def search(request):
                 meaning.append(meaning1)
 
 
-                meaning2 = soup.find_all('div', {'value': '2'})
-                if meaning2:
-                    meaning2 = meaning2[0].get_text(strip=True).split(':')[0]
-                    meaning.append(meaning2)
+            meaning2 = soup.find_all('div', {'value': '2'})
+            if meaning2:
+                meaning2 = meaning2[0].get_text(strip=True).split(':')[0]
+                meaning.append(meaning2)
 
 
-                meaning3 = soup.find_all('div', {'value': '3'})
-                if meaning3:
-                    meaning3 = meaning3[0].get_text(strip=True).split(':')[0]
-                    meaning.append(meaning3)
+            meaning3 = soup.find_all('div', {'value': '3'})
+            if meaning3:
+                meaning3 = meaning3[0].get_text(strip=True).split(':')[0]
+                meaning.append(meaning3)
 
 
-                meaning4 = soup.find_all('div', {'value': '4'})
-                if meaning4:
-                    meaning4 = meaning4[0].get_text(strip=True).split(':')[0]
-                    meaning.append(meaning4)
+            meaning4 = soup.find_all('div', {'value': '4'})
+            if meaning4:
+                meaning4 = meaning4[0].get_text(strip=True).split(':')[0]
+                meaning.append(meaning4)
                     
-                synonym_list = []
-                antonym_list = []
-                if source_synonym_antonym:
-                    soup2 = BeautifulSoup(source_synonym_antonym.text, 'html.parser')
+
+            if source_synonym_antonym:
+                soup2 = BeautifulSoup(source_synonym_antonym.text, 'html.parser')
 
                     # synonym_list = []
-                    synonyms = soup2.find_all('a', {'class': 'css-1kg1yv8 eh475bn0'})
+                synonyms = soup2.find_all('a', {'class': 'css-1kg1yv8 eh475bn0'})
 
-                    if synonyms:
-                        for i in synonyms:
-                            synonym_list.append(i.text)
-                    else:
-                        synonym_list.append("-1")
-        
-                    # antonym_list = []
-                    antonyms = soup2.find_all('a', {'class': 'css-15bafsg eh475bn0'})
-
-                    if antonyms:
-                        for i in antonyms:
-                            antonym_list.append(i.text)
-                    else:
-                        antonym_list.append("-1")
+                if synonyms:
+                    for i in synonyms:
+                        synonym_list.append(i.text)
                 else:
                     synonym_list.append("-1")
-            antonym_list.append("-1")
+        
+                    # antonym_list = []
+                antonyms = soup2.find_all('a', {'class': 'css-15bafsg eh475bn0'})
+
+                if antonyms:
+                    for i in antonyms:
+                        antonym_list.append(i.text)
+                else:
+                    antonym_list.append("-1")
+            else:
+                synonym_list.append("-1")
+                antonym_list.append("-1")
 
         else:
             synonym_list.append("-1")
