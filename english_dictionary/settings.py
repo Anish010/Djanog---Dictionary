4da94720c8d82276281f4c2a0db37c81361329c9
Anish Kumar Mohanty <72245764+Anish010@users.noreply.github.com>
2023-08-02 23:58:26 +0530

    Update settings.py
    added allowed hosts

--- english_dictionary/settings.py
+++ english_dictionary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-h$g%n(9mkg66r4rx19-l)093xs95_12hdkvdx=cktr=_i!&akx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '3.108.190.59', 'localhost', '127.0.0.1']
 
 
 # Application definition
